8bb1c722025ba012c3e954aaa8a713f510a652ce
Vitor Antonionne <114239671+vitorantonionne@users.noreply.github.com>
2023-01-19 11:06:02 -0300

    dinamismo com js

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-const form = document.querySelector('.clickbut')
-const nlwSetup = new NLWSETUP()
+const form = document.querySelector('#form-habits')
+const nlwSetup = new NLWSetup(form)
 
 
-const data () {
-    
+const data = {
+    coder: ["01-16", "01-17", "01-18", "01-19", "01-20", "01-21"],
+    training: ["01-17", "01-19", "01-20"],
+    games: ["01-17","01-19"]
 }
+
+
+nlwSetup.setData(data)
+nlwSetup.load()
