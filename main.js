02b702102bc6d4080edd2ee6ddad6f47b0706a20
Vitor Antonionne <114239671+vitorantonionne@users.noreply.github.com>
2023-01-20 12:05:18 -0300

    finalizado o js , adicionei varios comentarios para praticar .

--- main.js
+++ main.js
@@ -4,19 +4,37 @@ const nlwSetup = new NLWSetup(form)
 const button = document.querySelector('.clickbut')
 
 button.addEventListener('click', add)
+form.addEventListener('change', save)
 
 function add() {
-    const today = '01-01'
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5) 
+    // criar uma data , transformar em pt-br , recortar a data.
 
-    const dayExists = nlwSetup.dayExists(today)
+    // const today = "01/09"
 
-    if(dayExists) {
-        alert('data já inclusa')
+    const dayExists = nlwSetup.dayExists(today) 
+    // Aqui irá verificar na biblioteca  se haver o dia vai retornar true , se não haver o dia retorna false
+
+
+    if(dayExists)   {
+        alert('data já inclusa ❌') 
         return
-    }
+    }  // condição para adicinar ou não os dias
 
-    alert('Data adiconada com sucesso')
-    nlwSetup.addDay(todayz)
+        // se não tiver ,passa a informação como false ,dai o  dia passa direto na condição e é adicionado ... caso ja tenha o dia , vem a informção  como true ,dai ele valida o alert , para no return e não adiciona o dia 
+
+    alert('Data adiconada com sucesso 🆗')
+    nlwSetup.addDay(today) 
+    //addDay adiciona o dia 
+}
+
+
+function save() {
+    
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data)) 
+    // armazena as informações no local storage na chave NLWSetup@habis que pode receber strings , por isso usou o JSON.stringify para transformar o objeto nlwSetup.data em string e armazenar 
+
+    // e conforme for mudando o navegador de acesso , os dados não vão ser armazenador de forma universal para todos , pois cada navegador tem localstorage diferentes 
 }
 
 
@@ -27,5 +45,9 @@ function add() {
 // }
 
 
-// nlwSetup.setData(data)
-// nlwSetup.load()
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {} 
+// vai pegar as informações no local storage como strings dai vai converter para objeto usando json.parse.
+// || { } é quando passar o programa pela primeira vez vai está vazio , dai precisa de uma saida q é || {}(ou objeto vazio) 
+
+nlwSetup.setData(data) // armazena as const data
+nlwSetup.load() // carrega na pagina
