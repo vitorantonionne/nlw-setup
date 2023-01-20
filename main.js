9a384eae3ec8cc1596d8853d6d6e1ca08bc526f2
Vitor Antonionne <114239671+vitorantonionne@users.noreply.github.com>
2023-01-19 23:37:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
 
+const button = document.querySelector('.clickbut')
 
-const data = {
-    coder: ["01-16", "01-17", "01-18", "01-19", "01-20", "01-21"],
-    training: ["01-17", "01-19", "01-20"],
-    games: ["01-17","01-19"]
+button.addEventListener('click', add)
+
+function add() {
+    alert('ola mundo')
 }
 
 
-nlwSetup.setData(data)
-nlwSetup.load()
+// const data = {
+//     coder: ["01-16", "01-17", "01-18", "01-19", "01-20", "01-21"],
+//     training: ["01-17", "01-19", "01-20"],
+//     games: ["01-17","01-19"]
+// }
+
+
+// nlwSetup.setData(data)
+// nlwSetup.load()
